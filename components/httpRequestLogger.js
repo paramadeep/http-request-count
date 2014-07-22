9de21db4fd9d2734081e48393bc77dfae847cef0
deepak <anandhadeepak@gmail..com>
2014-07-22 15:29:49 +0530

    converted the loger to return count

--- components/httpRequestLogger.js
+++ components/httpRequestLogger.js
@@ -1,26 +1,79 @@
+var Ci = Components.interfaces;
+var Cr = Components.results;
+
 Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
+
+var HttpRequestCount = 0
 function HttpRequestLogger() {
   var httpRequestLogger =
   {
     observe: function(subject, topic, data) 
     {
       if (topic == "http-on-modify-request") {
-        var httpChannel = subject.QueryInterface(Components.interfaces.nsIHttpChannel);
-        var ref = httpChannel.referrer ? httpChannel.referrer.spec : "(none)";
-        var buffer = ref + " " + httpChannel.requestMethod + " " + httpChannel.URI.spec + "\n";
-        fos.write(buffer, buffer.length);
+        HttpRequestCount = HttpRequestCount + 1;
+        var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
+        var goodies = loadContextGoodies(httpChannel);
+        if (goodies) {
+          var myDocument = goodies.contentWindow.document;
+          var element =  myDocument.getElementById('httpcount');
+          if (element == null){
+            element = myDocument.createElement("HttpRequestCount");
+            element.setAttribute("id", "httpcount");
+          }
+          element.setAttribute("count",HttpRequestCount);
+          myDocument.documentElement.appendChild(element);
+        } else {
+          //dont do anything as there is no contentWindow associated with the httpChannel, liekly a google ad is loading or some ajax call or something, so this is not an error
+        }
       }
     }
   };
-  
-  var file = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("Desk", Components.interfaces.nsIFile);
-  file.append("http-request-log.txt");
-  var fos = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
-  // PR_WRONLY | PR_CREATE_FILE | PR_APPEND
-  fos.init(file, 0x02 | 0x08 | 0x10, -1, 0);
-  
+
   var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
   observerService.addObserver(httpRequestLogger, "http-on-modify-request", false);
+
+  //this function gets the contentWindow and other good stuff from loadContext of httpChannel
+  function loadContextGoodies(httpChannel) {
+    var loadContext;
+    try {
+      var interfaceRequestor = httpChannel.notificationCallbacks.QueryInterface(Ci.nsIInterfaceRequestor);
+      try {
+        loadContext = interfaceRequestor.getInterface(Ci.nsILoadContext);
+      } catch (ex) {
+        try {
+          loadContext = subject.loadGroup.notificationCallbacks.getInterface(Ci.nsILoadContext);
+        } catch (ex2) {}
+      }
+    } catch (ex0) {}
+
+    if (!loadContext) {
+      return null;
+    } else {
+      var contentWindow = loadContext.associatedWindow;
+      if (!contentWindow) {
+        return null;
+      } else {
+        var aDOMWindow = contentWindow.top.QueryInterface(Ci.nsIInterfaceRequestor)
+          .getInterface(Ci.nsIWebNavigation)
+          .QueryInterface(Ci.nsIDocShellTreeItem)
+          .rootTreeItem
+          .QueryInterface(Ci.nsIInterfaceRequestor)
+          .getInterface(Ci.nsIDOMWindow);
+        var gBrowser = aDOMWindow.gBrowser;
+        var aTab = gBrowser._getTabForContentWindow(contentWindow.top); 
+        var browser = aTab.linkedBrowser;
+        return {
+          aDOMWindow: aDOMWindow,
+            gBrowser: gBrowser,
+            aTab: aTab,
+            browser: browser,
+            contentWindow: contentWindow
+        };
+      }
+    }
+  }
+
+
 }
 
 HttpRequestLogger.prototype.classID = Components.ID('{c4a9bb50-b9b2-11e0-a4dd-0800200c9a66}');
